21299d953ece9816a2e4c58620c89821d4e6862b
Karen Barbé <info@karenbarbe.com>
2025-09-17 18:28:46 -0500

    add routes for users photos

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
   get("/users", { :controller => "users", :action => "index" })
   get("/users/:path_username", { :controller => "users", :action => "show" })
+  get("/users/:path_username/own_photos", { :controller => "users", :action => "own_photos" })
+  get("/users/:path_username/feed", { :controller => "users", :action => "feed" })
+  get("/users/:path_username/discover", { :controller => "users", :action => "discover" })
 
   # Routes for the Follow request resource:
 
