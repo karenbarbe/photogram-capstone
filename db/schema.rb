36573c0f70cccebb5babde17f50d5c713b45ff2d
Karen Barbé <info@karenbarbe.com>
2025-09-17 12:16:27 -0500

    generate Follow_request resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_17_171023) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_17_171549) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,6 +18,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_17_171023) do
     t.string "body"
     t.integer "photo_id"
     t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "follow_requests", force: :cascade do |t|
+    t.integer "recipient_id"
+    t.integer "sender_id"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
