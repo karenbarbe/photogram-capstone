39617367306d21be7ea97e1d9de4597fe80076d7
Karen Barbé <info@karenbarbe.com>
2025-09-17 18:14:11 -0500

    add route for users#show

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # Routes for Users
 
   get("/users", { :controller => "users", :action => "index" })
+  get("/users/:path_username", { :controller => "users", :action => "show" })
 
   # Routes for the Follow request resource:
 
