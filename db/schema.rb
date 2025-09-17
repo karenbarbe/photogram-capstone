2cf7885692cb66ee61cb14e69654c29f975bea62
Karen Barbé <info@karenbarbe.com>
2025-09-17 12:09:49 -0500

    generate Like resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_17_170734) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_17_170924) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "photo_id"
+    t.integer "fan_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "photos", force: :cascade do |t|
     t.text "caption"
