7b4bef5e1e1951bcfd7a7aeff435522bc287c577
Karen Barbé <info@karenbarbe.com>
2025-09-17 18:14:45 -0500

    add show action to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.all.order(:username)
     render({ :template => "user_templates/index" })
+  end
+
+  def show
+    the_username = params.fetch("path_username")
+
+    @matching_user = User.where({ :username => the_username }).first
+
+    render({ :template => "user_templates/show" })
   end
 end
