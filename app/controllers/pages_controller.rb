7ab74340bb7dc16b21b559346cc9cbf48f117257
Karen Barbé <info@karenbarbe.com>
2025-09-17 14:06:22 -0500

    add limited authentication to controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  skip_before_action(:authenticate_user!, { :only => [ :index ] })
+
   def index
     render({ :template => "page_templates/index" })
   end
