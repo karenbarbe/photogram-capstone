7a67a4ea284ff24e28076adb2240c9686fac7998
Karen Barbé <info@karenbarbe.com>
2025-09-18 14:38:05 -0500

    add logic to follow buttons and cancel/unfollow links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,27 @@ class UsersController < ApplicationController
 
   def show
     the_username = params.fetch("path_username")
-
     @matching_user = User.where({ :username => the_username }).first
 
-    render({ :template => "user_templates/show" })
+    if @matching_user.private? && !user_can_view_profile?(@matching_user)
+      redirect_to("/users", { :alert => "You're not authorized for that." })
+    else
+      render({ :template => "user_templates/show" })
+    end
+  end
+
+  private
+
+  def user_can_view_profile?(target_user)
+    # User can view if:
+    # 1. It's their own profile
+    # 2. The profile is public
+    # 3. They have an accepted follow request
+    return true if current_user == target_user
+    return true unless target_user.private?
+
+    # Check for accepted follow request
+    existing_request = target_user.received_follow_requests.find_by(sender: current_user)
+    existing_request&.status == "accepted"
   end
 end
