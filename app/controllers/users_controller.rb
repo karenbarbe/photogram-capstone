97ce16739375f85358c7d1b227c256633ad6820c
Karen Barbé <info@karenbarbe.com>
2025-09-19 19:13:41 -0500

    add skip authentication for "/users"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action(:authenticate_user!, { :only => [ :index ] })
   before_action :find_matching_user, only: [ :show, :own_photos, :liked_photos, :feed, :discover ]
 
   def index
